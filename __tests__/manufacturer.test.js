df9153172a2d2784472e54bdfd24dadd8610dd9f
Pete Hamrick <pete.hamrick@gmail.com>
2021-08-16 16:05:03 -0700

    passing /manufacturer get fetch test

--- __tests__/manufacturer.test.js
+++ __tests__/manufacturer.test.js
@@ -1,37 +1,37 @@
-// require('dotenv').config();
+require('dotenv').config();
 
-// const { execSync } = require('child_process');
+const { execSync } = require('child_process');
 
-// const fakeRequest = require('supertest');
-// const app = require('../lib/app');
-// const client = require('../lib/client');
+const fakeRequest = require('supertest');
+const app = require('../lib/app');
+const client = require('../lib/client');
 
-// const manufacturerData = require('../data/manufacturer.js');
+const manufacturerData = require('../data/manufacturer.js');
 
-// describe('manufacturer routes', () => {
+describe('manufacturer routes', () => {
 
-//   beforeAll(async () => {
-//     execSync('npm run setup-db');
+  beforeAll(async () => {
+    execSync('npm run setup-db');
 
-//     await client.connect();
+    await client.connect();
     
-//   }, 10000);
+  }, 10000);
 
-//   afterAll(done => {
-//     return client.end(done);
-//   });
+  afterAll(done => {
+    return client.end(done);
+  });
 
-//   test('GET /manufacturers returns a list of manufacturers', async() => {
-//     const expected = manufacturerData.map(maker => maker.name);
-//     const data = await fakeRequest(app)
-//       .get('/manufacturers')
-//       .expect('Content-Type', /json/)
-//       .expect(200);
+  test('GET /manufacturers returns a list of manufacturers', async() => {
+    const expected = manufacturerData.map(maker => maker.name);
+    const data = await fakeRequest(app)
+      .get('/manufacturers')
+      .expect('Content-Type', /json/)
+      .expect(200);
 
-//     const manufacturerNames = data.body.map(maker => maker.name);
+    const manufacturerNames = data.body.map(maker => maker.name);
 
-//     expect(manufacturerNames).toEqual(expected);
-//     expect(manufacturerNames.length).toBe(manufacturerNames.length);
-//     expect(data.body[0].id).toBeGreaterThan(0);
-//   });
-// });
+    expect(manufacturerNames).toEqual(expected);
+    expect(manufacturerNames.length).toBe(manufacturerNames.length);
+    expect(data.body[0].id).toBeGreaterThan(0);
+  });
+});
